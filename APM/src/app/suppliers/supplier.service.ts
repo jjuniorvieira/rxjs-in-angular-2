459f2cdbcb08614e9f0aa7010fa009e1f619d964
Junior <junior.jaava@gmail.com>
2021-05-09 14:07:48 +0200

    11.4 - High-order Observable - using concatMap

--- APM/src/app/suppliers/supplier.service.ts
+++ APM/src/app/suppliers/supplier.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { throwError, Observable } from 'rxjs';
+import { throwError, Observable, of } from 'rxjs';
+import { Supplier } from './supplier';
+import { concatMap, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,23 @@ import { throwError, Observable } from 'rxjs';
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  suppliersWithMap$ = of(1, 5, 8)
+    .pipe(
+      map(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`)
+      )
+    );
+
+  suppliersWithConcatMap$ = of(1, 5, 8)
+    .pipe(
+      tap(id => console.log('concatMap source Observable', id)),
+      concatMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
+  constructor(private http: HttpClient) {
+    this.suppliersWithConcatMap$.subscribe(
+      item => console.log('concatMap result', item)
+    )
+  }
 
   private handleError(err: any): Observable<never> {
     // in a real world app, we may send the server to some remote logging infrastructure
